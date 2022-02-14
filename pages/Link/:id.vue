8d9af0f757b77a40aad0891483454b17ed06b5d8
Antonio Celaya <antonio.celaya@metafinanciera.com>
2022-02-14 15:39:11 -0600

    link expo

---
+++ pages/Link/:id.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OpenLinking',
+  mounted () {
+    this.openLink()
+  },
+  methods: {
+    openLink () {
+      console.log(this.$route.params)
+      window.open('almacenmeta://' + this.$route.params.id, '_self')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
